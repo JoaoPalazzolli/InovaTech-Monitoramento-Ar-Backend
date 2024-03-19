2df28db4000c60a3eada686543c3258015c0e15e
João Pedro <joaopedropalazzolli@gmail.com>
2024-03-18 22:47:27 -0300

    Deletado a classe AmbienteDBConfig.java

--- src/main/java/com/project/Main.java
+++ src/main/java/com/project/Main.java
@@ -1,12 +1,19 @@
 package com.project;
 
-import com.project.configs.AmbienteDBConfig;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        AmbienteDBConfig.openConnection();
-        AmbienteDBConfig.gerarTabelasDB();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-hibernate-mysql");
+        EntityManager em = emf.createEntityManager();
+
+        System.out.println(em.isOpen());
+
+        em.close();
+        emf.close();
     }
 }
