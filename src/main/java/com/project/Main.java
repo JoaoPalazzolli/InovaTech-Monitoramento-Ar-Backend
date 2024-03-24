4c52c2d5a563123f428cf9ef353b54ec1f44eb51
João Palazzolli <joaopedropalazzolli@gmail.com>
2024-03-24 13:57:44 -0300

    Main vazia

--- src/main/java/com/project/Main.java
+++ src/main/java/com/project/Main.java
@@ -1,19 +1,9 @@
 package com.project;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
-
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-hibernate-mysql");
-        EntityManager em = emf.createEntityManager();
 
-        System.out.println(em.isOpen());
-
-        em.close();
-        emf.close();
     }
 }
